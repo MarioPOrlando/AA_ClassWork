de7398937510528dfe660ffe8a237074f2ff66a5
Shane <shane.messmer1@gmail.ecom>
2021-02-08 17:12:13 -0500

    Knight Moves base

--- W3D5/knights_travails.rb
+++ W3D5/knights_travails.rb
@@ -1,2 +1,24 @@
-class PolyTreeNode
+class KnightPathFinder
+  def initialize
+    @position = [0,0]
+    @find_path = self.root_node
+    @considered_positions
+
+
+  end
+
+  def self.root_node 
+    @position
+  end
+
+  def build_move_tree
+  end
+
+  def self.valid_moves(pos)
+  
+  
+  end
+
 end
+
+
