d0b15bda96e3435e63f505712e90a77e5a9840f3
Shane <shane@Shanes-iMac.Home>
2021-02-08 20:07:58 -0500

    Finishes part I of Knights Travails project

--- W3D5/knights_travails.rb
+++ W3D5/knights_travails.rb
@@ -1,21 +1,38 @@
 require_relative "poly_node.rb"
 
+
 class KnightPathFinder
+attr_reader :considered_positions
+
   def initialize
     @position = [0,0]
     @root_node = PolyTreeNode.new(@position)
     @considered_positions = []
+    build_move_tree 
   end
 
   def build_move_tree
+    nodes = [@root_node]
+    until nodes.empty?
+      node = nodes.shift
+      current_position = node.value
+      new_move_positions(current_position).each do |children|
+        child = PolyTreeNode.new(children)
+        node.add_child(child)
+        nodes << child 
+      end
+    end
+
+
+
 
   end
 
     def self.valid_moves(pos)
-
+        valid_moves = []
         x = pos[0]
         y = pos[1]
-        valid_moves = []
+       
         possible_moves =[[1, 2], [2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1], [-1, 2], [-2, 1]]
             possible_moves.each do |sub_arr|
             new_x = sub_arr[0] + x
@@ -27,6 +44,20 @@ class KnightPathFinder
         valid_moves
     end
 
+
+    def new_move_positions(pos)
+      possible_moves = KnightPathFinder.valid_moves(pos)
+      newvar = possible_moves.select {|spot| !@considered_positions.include?(spot)}
+      @considered_positions += newvar
+      newvar
+    end
+
+
+
+
+
 end
 
-KnightPathFinder.valid_moves
+jeff = KnightPathFinder.new
+
+print jeff.considered_positions.length
