f6fabdc1cada20bbe779734332f07568d0672bca
MarioPOrlando <Orlandomario73@yahoo.com>
2021-02-09 16:01:42 -0800

    Finished Project

--- W4D2/class_in.rb
+++ W4D2/class_in.rb
@@ -1,33 +1,41 @@
 class Employee
- 
-    def initialize(name, title, salary, boss)
+
+  attr_reader :name, :salary
+
+    def initialize(name, title, salary, boss = nil)
         @name = name
         @title = title
         @salary = salary
         @boss = boss 
+        @boss.add_employee(self) if boss != nil
     end
 
     def bonus(multiplyer)
       employ_bonus =  @salary * multiplyer
     end
 
+    def inspect
+        "Name = #{self.name} "
+    end
 
 end
 
 class Manager < Employee
 
+    attr_reader :employee_list, :name, :salary
     
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary, boss = nil)
         super(name, title, salary, boss)
-        @name = name
-        @title = title
-        @salary = salary
-        @boss = boss 
         @employee_list = []
+        # @boss.add_employee if boss != nil
+    end
+    
+    def inspect
+        "Name = #{self.name} Employee List = #{self.employee_list} "
     end
 
-    def add_employee
-        @employee_list << Employee
+    def add_employee(employee)
+        @employee_list << employee
     end
 
     def bonus(multiplyer)
@@ -39,15 +47,19 @@ class Manager < Employee
         total_salary = 0
         until queue.empty?
             current_employee = queue.shift
-           total_salary += node.salary if node != self
+           total_salary += current_employee.salary if current_employee != self
            @employee_list.each do |employee|
             queue << employee
            end
         end
         total_salary   
     end
-
-
 end
 
- 
+ ned = Manager.new("Ned", "Founder", 1000000)
+
+ darren = Manager.new("Darren", "TA Manager", 78000, ned)
+ shawna = Employee.new("Shawna", "TA", 12000, darren)
+ david = Employee.new("David", "TA", 10000, darren)
+ p darren.bonus(4)
+# p ned.employee_list
