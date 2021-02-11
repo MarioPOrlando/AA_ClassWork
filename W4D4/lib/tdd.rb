41969ce9915976923e5d0e20baed14fe69551255
MarioPOrlando <Orlandomario73@yahoo.com>
2021-02-11 14:51:32 -0800

    two sums finsihed

--- W4D4/lib/tdd.rb
+++ W4D4/lib/tdd.rb
@@ -4,4 +4,32 @@ def my_uniq(arr)
     uniq
 end
 
-p my_uniq([1,1,2,2,3,4])
+# p my_uniq([1,1,2,2,3,4])
+
+
+
+def two_sum(arr)
+    new_arr = []
+
+    arr.each_with_index do |ele1, i|
+        arr.each_with_index do |ele2, j|
+        if i < j 
+            if ele1 + ele2 == 0 
+                new_arr << [i, j] 
+            end
+        end
+        end
+    end
+    new_arr
+end
+
+# def two_sum(arr)
+#     pairs = []
+
+#     arr.each_index do |i|
+#         ((i + 1)...arr.length).to_a.each do |j|
+#             paris << [i, j] if arr[i] + arr[j] == 0
+#         end
+#     end
+#     pairs
+# end
--- W4D4/spec/tdd_spec.rb
+++ W4D4/spec/tdd_spec.rb
@@ -19,10 +19,14 @@ end
 
 
 describe "two_sum" do
-    let(:array) { [1, 5, -2, 1] }
+    let(:array) { [1, 5, -2, -1] }
+    let(:multi_pair) { [1, -1, 2, -2] }
+    let(:no_pair) {[1, 2, 3, 4]}
 
     it "finds a zero sum pair" do
-        expect(two_sum(array)).to eq()
+        expect(two_sum(array)).to eq([[0,3]])
+        expect(two_sum(multi_pair)).to eq([[0, 1], [2, 3]])
+        expect(two_sum(no_pair)).to eq([])
     end
 
 end
