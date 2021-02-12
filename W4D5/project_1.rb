c343dfd4c77cf2b3603990dca72adf11ff04b86c
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-02-12 14:06:01 -0800

    Add largest_cont_subsum

--- W4D5/project_1.rb
+++ W4D5/project_1.rb
@@ -1,13 +1,13 @@
-# def my_min(arr)
-#    min = arr[0]
+def my_min(arr)
+   min = arr[0]
 
-#    arr.each do |ele|
-#         if ele < min
-#             min = ele
-#         end
-#     end
-#    min
-# end
+   arr.each do |ele|
+        if ele < min
+            min = ele
+        end
+    end
+   min
+end
 #Big O: O(n)
 
 def my_min(arr)
@@ -17,6 +17,27 @@ def my_min(arr)
 end
 #Big O: O(n ^ 2)
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+def largest_cont_subsum(list)
+    pairs = []
+
+    list.each.with_index do |n_1, i_1|
+        (i_1...list.length).each do |i_2|
+            pairs << list[i_1..i_2]
+        end
+    end
+    sums = []
+    pairs.each do |pair|
+        sums << pair.sum
+    end
+    sums.length
+end
+list = [5, 3, -7, 9, 18, 11]
+p largest_cont_subsum(list)
+#Big O: 3n^3 = O(2^n)
+
+
+
 
