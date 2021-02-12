b3a30ba5bcf308777d3a2e14633f97d7dbf3e814
MarioPOrlando <Orlandomario73@yahoo.com>
2021-02-12 13:34:06 -0800

    My min finsihed

--- W4D5/project_1.rb
+++ W4D5/project_1.rb
@@ -1,14 +1,22 @@
-def my_min(arr)
-   min = arr[0]
+# def my_min(arr)
+#    min = arr[0]
 
-   arr.each do |ele|
-        if ele < min
-            min = ele
-        end
+#    arr.each do |ele|
+#         if ele < min
+#             min = ele
+#         end
+#     end
+#    min
+# end
+#Big O: O(n)
+
+def my_min(arr)
+    arr.each_with_index do |ele1, idx1|
+     return ele1 if arr.all? {|n| ele1 < n}             
     end
-   min
 end
+#Big O: O(n ^ 2)
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)  # =>  -5
-#Big O: 
+
