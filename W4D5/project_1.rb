5c9e417c2c469b0e85c88ca1599b3c79b578127a
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-02-12 15:00:11 -0800

    Add phase 2 of sums

--- W4D5/project_1.rb
+++ W4D5/project_1.rb
@@ -20,37 +20,37 @@ end
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
 
-# def largest_cont_subsum(list)
-#     pairs = []
+def largest_cont_subsum(list)
+    pairs = []
 
-#     list.each.with_index do |n_1, i_1|
-#         (i_1...list.length).each do |i_2|
-#             pairs << list[i_1..i_2]
-#         end
-#     end
-#     sums = []
-#     pairs.each do |pair|
-#         sums << pair.sum
-#     end
-#     sums.length
-# end
+    list.each.with_index do |n_1, i_1|
+        (i_1...list.length).each do |i_2|
+            pairs << list[i_1..i_2]
+        end
+    end
+    sums = []
+    pairs.each do |pair|
+        sums << pair.sum
+    end
+    sums.length
+end
 # list = [5, 3, -7, 9, 18, 11]
 # p largest_cont_subsum(list)
 #Big O: 3n^3 = O(2^n)
 
 def largest_cont_subsum(list)
-    biggest = list
-    current = list[1]
+    biggest = list[0]
+    current = 0
 
     (0...list.length).each do |i|
-        if current <  list[i] + list[i + 1] && i != list.length - 2
-            puts current
-            current = list[i] + list[i + 1] 
-            puts current
-        end
+           
+            current += list[i]
+            biggest = current if current > biggest
+    
     end
-    current
+    biggest
 end
 
-list = [5, 3, -7,]
-p largest_cont_subsum(list)
+# list = [5, 3, -7]
+# p largest_cont_subsum(list)
+#Big O: n 
