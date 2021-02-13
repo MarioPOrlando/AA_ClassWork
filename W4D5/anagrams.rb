b1b3159c4eb01b87dd75dde37e7e0db0941d62ed
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-02-12 16:05:01 -0800

    Create second anagram method for phase 2

--- W4D5/anagrams.rb
+++ W4D5/anagrams.rb
@@ -1,12 +1,27 @@
-def anagram?(str_1,str_2)
-    str1 = str_1.split('')
-    str2 = str_2.split('')
-    anagram = []
-    anagram = str2.permutation(str1.length).to_a
-    anagram.include?(str1)
-    p anagram.length
-end
+# def anagram?(str_1,str_2)
+#     str1 = str_1.split('')
+#     str2 = str_2.split('')
+#     anagram = []
+#     anagram = str2.permutation(str1.length).to_a
+#     anagram.include?(str1)
+#     p anagram.length
+# end
 #Big: O(n ^ 2)
 
+# p anagram?("gizmo", "sally")    #=> false
+# p anagram?("elvis", "lives")    #=> true
+
+def anagram?(str_1, str_2)
+    str1 = str_1.split("")
+    str2 = str_2.split("")
+
+    str1.each do |ele|
+        index = str2.find_index(ele)
+        str2.delete(index)
+    end
+
+    str2.empty?
+end
+
 p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+p anagram?("elvis", "lives") 
