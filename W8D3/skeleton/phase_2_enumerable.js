0979b285f097fea601f4cb8c24e1ff6b829e4d2a
MarioPOrlando <Orlandomario73@yahoo.com>
2021-03-11 10:16:36 -0800

    enumerables

--- W8D3/skeleton/phase_2_enumerable.js
+++ W8D3/skeleton/phase_2_enumerable.js
@@ -1,1 +1,27 @@
-Array.prototype.twoSum = function()
+// Array.prototype.myEach = function (callback) {
+//     for (let i = 0; i < this.length; i++) {
+//         callback(this[i]);
+//     };
+// };
+
+
+
+Array.prototype.myMap = function (callback){
+    let newArr = [];
+    for (let i = 0; i < this.length; i++){
+        newArr.push(this[i]);
+    };
+    callback(newArr);
+};
+
+
+
+
+
+
+
+
+[1,2,3,4,5].myMap(function(Array) {
+    console.log(Array)
+});
+
